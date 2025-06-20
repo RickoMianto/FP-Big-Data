2f3208dfc09a4bbc6fa41aa495d7c02f9a93786d
RickoMianto <saputraricko0635@gmail.com>
2025-06-20 12:34:09 +0700

    commit

--- spark_processor/jobs/bronze_to_silver.py
+++ spark_processor/jobs/bronze_to_silver.py
@@ -21,6 +21,13 @@ class BronzeToSilverProcessor:
                        "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,"
                        "org.apache.hadoop:hadoop-aws:3.3.4,"
                        "com.amazonaws:aws-java-sdk-bundle:1.12.376") \
+                .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:9000") \
+                .config("spark.hadoop.fs.s3a.access.key", "minioadmin") \
+                .config("spark.hadoop.fs.s3a.secret.key", "minioadmin") \
+                .config("spark.hadoop.fs.s3a.path.style.access", "true") \
+                .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+                .config("spark.sql.adaptive.enabled", "true") \
+                .config("spark.sql.adaptive.coalescePartitions.enabled", "true") \
                 .config("spark.sql.adaptive.enabled", "true") \
                 .config("spark.sql.adaptive.coalescePartitions.enabled", "true") \
                 .getOrCreate()
